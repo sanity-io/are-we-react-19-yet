8001e667316a9fc7ae30051534c808e5de46170b
Cody Olsen <stipsan@gmail.com>
2024-05-24 17:59:10 +0200

    add live subscription

--- website/app/(report)/page.tsx
+++ website/app/(report)/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({
 }: {
   searchParams: {[key: string]: string | string[] | undefined}
 }) {
-  const [report] = await sanityFetch<ReportQueryResult>({
+  const [report, LiveSubscription] = await sanityFetch<ReportQueryResult>({
     query: reportQuery,
     lastLiveEventId,
   })
@@ -46,6 +46,7 @@ export default async function Page({
           </p>
         </footer>
       )}
+      <LiveSubscription />
     </>
   )
 }
