e4241255b2809b9c245e7e2a164cd5ede9dd615a
Cody Olsen <stipsan@gmail.com>
2024-05-24 17:56:01 +0200

    render the report

--- website/app/(report)/page.tsx
+++ website/app/(report)/page.tsx
@@ -19,18 +19,33 @@ export default async function Page({
       <main className="container mx-auto px-5">
         <section className="mb-16 mt-16 flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
           <h1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">
-            Yes or no
+            {report && report.total > 0 && report.passing === report.total ? ' ✨ YES ✨' : 'No'}
           </h1>
-          <h2 className="mt-5 text-pretty text-center text-lg lg:pl-8 lg:text-left">
-            0 of x tests are passing (0%)
-          </h2>
+          {report && report.total > 0 && (
+            <h2 className="mt-5 text-pretty text-center text-lg lg:pl-8 lg:text-left">
+              {report.passing} of {report.total} tests are passing (
+              {`${Math.floor((report.passing / report.total) * 100)}%`})
+            </h2>
+          )}
         </section>
+        {report?.test.map((test) => {
+          return (
+            <a
+              key={test._key}
+              href={test.name ? `https://npmjs.com/package/${test.name}` : undefined}
+            >
+              {test.name || 'No name'}
+            </a>
+          )
+        })}
       </main>
-      <footer className="container mx-auto px-5">
-        <p className="text-muted text-center text-sm">
-          Last update: <DateComponent dateString="2024-05-24T14:50:15.111Z" />
-        </p>
-      </footer>
+      {report?._createdAt && (
+        <footer className="container mx-auto px-5">
+          <p className="text-muted text-center text-sm">
+            Last update: <DateComponent dateString={report._createdAt} />
+          </p>
+        </footer>
+      )}
     </>
   )
 }
--- website/sanity/lib/queries.ts
+++ website/sanity/lib/queries.ts
@@ -3,7 +3,11 @@ import {groq} from 'next-sanity'
 export const reportQuery = groq`
   *[_type == "report" && defined(test)] | order(_createdAt desc)[0]{
     _createdAt,
-    "test": coalesce(test[]{name,pass}, []),
+    "test": coalesce(test[]{
+      _key,
+      name,
+      "pass": coalesce(pass, false)
+    }, []),
     "total": coalesce(count(test), 0),
     "passing": coalesce(count(test[pass == true]), 0),
   }
