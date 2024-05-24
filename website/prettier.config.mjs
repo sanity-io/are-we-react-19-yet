b41ffb3beeb6c314ed9880a42b68a9f3503e4a21
Cody Olsen <stipsan@gmail.com>
2024-05-24 14:36:58 +0200

    chore: update ignore

--- website/prettier.config.mjs
+++ website/prettier.config.mjs
@@ -2,8 +2,8 @@ import defaults from '@sanity/prettier-config'
 
 /** @type {import("prettier").Config} */
 const config = {
-    ...defaults,
-    plugins: [...defaults.plugins, 'prettier-plugin-tailwindcss'],
+  ...defaults,
+  plugins: [...defaults.plugins, 'prettier-plugin-tailwindcss'],
 }
 
 export default config
