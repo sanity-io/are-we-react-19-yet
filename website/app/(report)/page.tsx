a97a32a2de2116931dc1ff0f191dc7ce5870e618
Cody Olsen <stipsan@gmail.com>
2024-05-24 18:09:56 +0200

    update view

--- website/app/(report)/page.tsx
+++ website/app/(report)/page.tsx
@@ -1,3 +1,5 @@
+import {CheckmarkCircleIcon, CloseCircleIcon} from '@sanity/icons'
+
 import type {ReportQueryResult} from '@/sanity.types'
 import {reportQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/fetch'
@@ -28,16 +30,26 @@ export default async function Page({
             </h2>
           )}
         </section>
-        {report?.test.map((test) => {
-          return (
-            <a
-              key={test._key}
-              href={test.name ? `https://npmjs.com/package/${test.name}` : undefined}
-            >
-              {test.name || 'No name'}
-            </a>
-          )
-        })}
+        <section className="grid grid-cols-1 gap-4 bg-slate-50 p-4 text-sm leading-6 sm:grid-cols-2 sm:px-8 sm:pb-8 sm:pt-6 lg:grid-cols-1 lg:p-4 xl:grid-cols-2 xl:px-8 xl:pb-8 xl:pt-6">
+          {report?.test.map((test) => {
+            return (
+              <a
+                key={test._key}
+                className="g-blue-500 group relative rounded-md bg-white p-3 shadow-sm ring-1 ring-slate-200 hover:shadow-md hover:ring-blue-500"
+                rel="noreferrer noopener"
+                target="_blank"
+                href={test.name ? `https://npmjs.com/package/${test.name}` : undefined}
+              >
+                <span
+                  className={`absolute right-1.5 top-1.5 -z-0 scale-150 text-4xl opacity-25 ${test.pass ? 'text-green-800' : 'text-red-800'}`}
+                >
+                  {test.pass ? <CheckmarkCircleIcon /> : <CloseCircleIcon />}
+                </span>
+                <span className="relative z-10">{test.name || 'No name'}</span>
+              </a>
+            )
+          })}
+        </section>
       </main>
       {report?._createdAt && (
         <footer className="container mx-auto px-5">
