e361109499f417d66c966535c64b0c6d331af026
Cody Olsen <stipsan@gmail.com>
2024-05-24 14:35:06 +0200

    feat: move folder

---
+++ website/prettier.config.mjs
@@ -0,0 +1,9 @@
+import defaults from '@sanity/prettier-config'
+
+/** @type {import("prettier").Config} */
+const config = {
+    ...defaults,
+    plugins: [...defaults.plugins, 'prettier-plugin-tailwindcss'],
+}
+
+export default config
