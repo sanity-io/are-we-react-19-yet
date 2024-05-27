43d5dd88ab74c5ba2a6118612e94bda43b5fe256
Cody Olsen <stipsan@gmail.com>
2024-05-27 14:22:18 +0200

    setup the upload script

--- test/upload.js
+++ test/upload.js
@@ -1,12 +1,33 @@
 // @ts-check
 
 import {writeFile, stat, mkdir, readFile} from 'node:fs/promises'
+import {createRequire} from 'node:module'
+import {createClient} from '@sanity/client'
+
+const require = createRequire(import.meta.url)
 
 const token = process.env.SANITY_TOKEN
 if (!token) {
   console.log('Please provide a SANITY_TOKEN')
   process.exit(1)
 }
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+if (!projectId) {
+  console.log('Please provide a NEXT_PUBLIC_SANITY_PROJECT_ID')
+  process.exit(1)
+}
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+if (!dataset) {
+  console.log('Please provide a NEXT_PUBLIC_SANITY_DATASET')
+  process.exit(1)
+}
+const client = createClient({
+  projectId,
+  dataset,
+  token,
+  useCdn: false,
+  apiVersion: '2024-05-27',
+})
 
 const {testResults} = JSON.parse((await readFile('./result.json', 'utf8')).toString())
 
@@ -16,4 +37,18 @@ if (!pnpmTests) {
   process.exit(1)
 }
 const {assertionResults} = pnpmTests
-console.log(assertionResults)
+const result = []
+for (const {title, status} of assertionResults) {
+  const pkgResult = {_key: title, name: title, pass: status === 'passed'}
+  const version = require(`./fixtures/${title}/package.json`).dependencies[title]
+  Object.assign(pkgResult, {version})
+  result.push(pkgResult)
+}
+const document = {
+  _id: new Date().toJSON().split('T')[0],
+  _type: 'report',
+  test: result,
+}
+console.log('About to create document:', document)
+const response = await client.createOrReplace(document)
+console.log('Document created: ', response)
