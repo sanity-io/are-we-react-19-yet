8375f5097702636c8b571915f882b8779788c4df
Cody Olsen <stipsan@gmail.com>
2024-05-28 10:22:54 +0200

    stop concurrency

--- test/pnpm.test.ts
+++ test/pnpm.test.ts
@@ -4,13 +4,14 @@ import {execa} from 'execa'
 
 const pkgfiles = await glob('fixtures/**/package.json', {ignore: '**/node_modules/**'})
 
-describe.concurrent('pnpm install --resolution-only', () => {
+describe('pnpm install --resolution-only', () => {
   test.each(pkgfiles.map((pkg) => pkg.replace(/^fixtures\//, '').replace(/\/package.json$/, '')))(
     '%s',
     async (pkg) => {
       const {stdout} = await execa({
         cwd: `fixtures/${pkg}`,
       })`pnpm install --resolution-only --no-frozen-lockfile`
+      expect(stdout).toMatchSnapshot()
       expect(stdout).not.toMatch('unmet peer react')
     },
   )
